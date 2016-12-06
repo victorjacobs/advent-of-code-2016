533aea074f1819a018dd28660e8fbe4d7203b567
Victor Jacobs <victor.jacobs@me.com>
2016-12-06 22:16:02 +0100

    Day 4: part 1

--- 4.securityThroughObscurity/part1.hs
+++ 4.securityThroughObscurity/part1.hs
@@ -1,6 +1,5 @@
 import Data.List.Extra
 import qualified Data.Map.Strict as Map
-import Data.Ord
 
 type Room = (String, Int, String)
 type CharOccurrences = Map.Map Char Int
@@ -8,18 +7,30 @@ type CharOccurrences = Map.Map Char Int
 main :: IO ()
 main = do
     content <- readFile "data.txt"
-    let ls = lines $ trim content
+    let ls = foldl (+) 0 $ map sectorId $ filter isCorrectChecksum $ map parseLine $ lines $ trim content
     print ls
 
 parseLine :: String -> Room
-parseLine str = (name, sector, checksum)
+parseLine str = (name, sector, chk)
     where splitOnDashes = splitOn "-" str
           lastPartSplitOnBracket = splitOn "[" $ last splitOnDashes
           sector = read $ head lastPartSplitOnBracket
-          checksum = init $ last lastPartSplitOnBracket
+          chk = init $ last lastPartSplitOnBracket
           name = intercalate "" $ init splitOnDashes
+
+isCorrectChecksum :: Room -> Bool
+isCorrectChecksum (name, _, chk) = (checksum $ characterOccurrences name Map.empty) == chk
+
+sectorId :: Room -> Int
+sectorId (_, sid, _) = sid
 
 characterOccurrences :: String -> CharOccurrences -> CharOccurrences
 characterOccurrences (x:xs) occurrences = characterOccurrences xs updatedOccurrences
     where updatedOccurrences = Map.insertWith (+) x 1 occurrences
 characterOccurrences [] occurrences = occurrences
+
+checksum :: CharOccurrences -> String
+checksum occurrences = take 5 $ foldr ((++) . sort) "" $ map snd $ Map.toDescList $ Map.foldlWithKey insertCharacterByCount Map.empty occurrences
+
+insertCharacterByCount :: Map.Map Int String -> Char -> Int -> Map.Map Int String
+insertCharacterByCount charsByCount char count = Map.insertWith (++) count [char] charsByCount
