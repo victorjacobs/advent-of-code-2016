f72b79546877a9652295e41f33f5155d58691a39
Victor Jacobs <victor.jacobs@me.com>
2016-12-24 18:08:34 +0100

    Day 8 done

--- 8.twoFactor/part1.hs
+++ 8.twoFactor/part1.hs
@@ -1,4 +1,3 @@
-import Text.Regex.PCRE
 import Data.List.Extra
 import qualified Data.Map.Strict as Map
 
@@ -14,8 +13,8 @@ data Instruction =
 main :: IO ()
 main = do
     content <- readFile "data.txt"
-    let ls = lines $ trim content
-    print $ length ls
+    let finalState = foldl evaluateInstruction Map.empty $ map parseLine $ lines $ trim content
+    print $ length $ Map.filter id finalState
 
 parseLine :: String -> Instruction
 parseLine str = parseLineParts $ splitOn " " str
@@ -30,6 +29,16 @@ parseLineParts ["rotate", direction, position, _, amount]
               amountParsed = read amount
 
 evaluateInstruction :: State -> Instruction -> State
-evaluateInstruction state (Rectangle x2 y2) =
-    foldr (Map.alter (\_ -> Just True)) state [(x, y) | x <- [0..(x2 - 1)], y <- [0..(y2 - 1)]]
--- evaluateInstruction state (RotateColumn pos amount) =
+evaluateInstruction state (Rectangle x2 y2) = foldr (`Map.insert` True) state [(x, y) | x <- [0..(x2 - 1)], y <- [0..(y2 - 1)]]
+evaluateInstruction state (RotateRow row amount) = foldl (rotateRowElement amount) state rowElements
+    where rowElements = [(pos, Map.lookup pos state) | x <- [0..49], let pos = (x, row)]
+evaluateInstruction state (RotateColumn column amount) = foldl (rotateColumnElement amount) state columnElements
+    where columnElements = [(pos, Map.lookup pos state) | y <- [0..5], let pos = (column, y)]
+
+rotateRowElement :: Int -> State -> (Pos, Maybe Bool) -> State
+rotateRowElement amount state ((x, y), Just bool) = Map.insert ((x + amount) `mod` 50, y) bool state
+rotateRowElement amount state ((x, y), Nothing) = Map.insert ((x + amount) `mod` 50, y) False state
+
+rotateColumnElement :: Int -> State -> (Pos, Maybe Bool) -> State
+rotateColumnElement amount state ((x, y), Just bool) = Map.insert (x, (y + amount) `mod` 6) bool state
+rotateColumnElement amount state ((x, y), Nothing) = Map.insert (x, (y + amount) `mod` 6) False state
